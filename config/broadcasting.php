719850d23f62393eae4ee19ceef54d4fc5c517d1
badhded@gmail.com <badhded@gmail.com>
2025-08-18 09:00:18 +0300

    Update package.json to include laravel-echo and pusher-js dependencies. Refactor broadcasting configuration to simplify Pusher options and enforce TLS usage.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,19 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusherapp.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ],
-            ],
-            'client_options' => [
-                // Guzzle client options
-                'verify' => false,
+                'useTLS' => true,
             ],
         ],
 
